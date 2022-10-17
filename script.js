848df53cb790a696f301a08db0afd4a799207620
Snack-Time <wmcnally1226@gmail.com>
2022-10-17 02:11:35 -0400

    put in buttons and some css styling

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function operate(operator, numOne, numTwo) {
+    switch (operator) {
+        case '+':
+            add(numOne, numTwo);
+            return;
+        case '-':
+            subtract(numOne, numTwo);
+            return;
+        case '*':
+            multiply(numOne, numTwo);
+            return;
+        case '%':
+            if (b === 0) {
+                return null; }
+            else {
+                return divide(numOne, numTwo)};
+            }
+
+}
+
 function add(x, y) {
     return x + y;
 };
