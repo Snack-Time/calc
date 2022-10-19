285d688398f7586026ae6a9553d920b6980d9569
Snack-Time <wmcnally1226@gmail.com>
2022-10-19 18:18:11 -0400

    working on wiring functionality

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/* Query Selectors */
+
+const calcContainer = document.querySelector("#calculator-container");
+const displayContainer = document.querySelector("#display-container");
+const btnContainer = document.querySelector("#button-container");
+const numBtn = document.querySelectorAll(".numButton");
+const opBtn = document.querySelectorAll(".operatorButton");
+const oldOperandDisplay = document.querySelector("#oldOperand");
+const newOperandDisplay = document.querySelector("#newOperand");
+let oldNum = "";
+let newNum = "";
+let holderNum = "";
+let operator = null;
+
 function operate(operator, numOne, numTwo) {
     switch (operator) {
         case '+':
@@ -10,13 +24,14 @@ function operate(operator, numOne, numTwo) {
             multiply(numOne, numTwo);
             return;
         case '%':
-            if (b === 0) {
+            if (numTwo === 0) {
+                alert("You can't divide by zero!")
                 return null; }
             else {
                 return divide(numOne, numTwo)};
-            }
+            };
 
-}
+};
 
 function add(x, y) {
     return x + y;
@@ -24,12 +39,39 @@ function add(x, y) {
 
 function subtract(x, y) {
     return x - y;
-}
+};
 
 function multiply(x, y) {
     return x * y;
-}
+};
 
 function divide(x, y) {
     return x / y;
+};
+
+numBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (newOperandDisplay.textContent.includes(".") && button.textContent === ".") return;
+        appendNumber(button.textContent);
+    });
+});
+
+
+
+opBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        appendOperation(button.textContent);
+    });
+});
+
+function appendNumber(button) {
+    newNum = newNum.toString() + button.toString();
+    newOperandDisplay.textContent = newNum;
 }
+
+function appendOperation(button) {
+    operator = button;
+    oldOperandDisplay.textContent = `${newNum} ${operator}`;
+    newNum = oldNum;
+    newNum = "";
+};
